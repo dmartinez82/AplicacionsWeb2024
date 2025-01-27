f2516c6324bc1e241bb5f6f5ae501a208c0d2dc3
Daniel Martínez <dmartinez@esliceu.com>
2025-01-27 19:21:02 +0100

    exercicis javascript

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,10 @@
 const boton1 = document.querySelector("#boton1");
+const btnSaludo = document.querySelector("#btnSaludo");
+const footer = document.querySelector("footer");
+const mensaje = document.querySelector("#mensaje");
 
 boton1.onclick = tocame;
+btnSaludo.onclick = saludo;
 
 let contador = 0;
 
@@ -8,4 +12,12 @@ function tocame(){
     contador++;
     var veces = (contador==1)?"vez":"veces";
     boton1.textContent = `Me han tocado ${contador} ${veces}!`;
+}
+
+function saludo(){
+    var autor = footer.textContent;
+    autor = autor.trim().replace("©", "");
+    mensaje.textContent = "Hola" + autor;
+    mensaje.style.color = "red";
+    mensaje.classList.add("fondoVerde");
 }
