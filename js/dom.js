cf5918a5b2d96a5b262d2e73996613e9567c18c7
Daniel Martínez <dmartinez@esliceu.com>
2025-02-03 18:29:31 +0100

    avanços JS

--- js/dom.js
+++ js/dom.js
@@ -21,3 +21,58 @@ function saludo(){
     mensaje.style.color = "red";
     mensaje.classList.add("fondoVerde");
 }
+
+function calcula(){
+
+    var primer = document.querySelector("#primer").value;
+    var segon = document.querySelector("#segon").value;
+    var operacio = document.querySelector("#operacions").value;
+
+    if(primer == "") primer = 0;
+    if(segon == "") segon = 0;
+
+    primer = parseInt(primer);
+    segon = parseInt(segon);
+
+    var resultadoOperacion;
+    if (operacio == "+") resultadoOperacion = suma(primer, segon);
+    else if (operacio == "-") resultadoOperacion = resta(primer, segon);
+    else if (operacio == "*") resultadoOperacion = mult(primer, segon);
+    else if (operacio == "/") resultadoOperacion = div(primer, segon);
+    else if (operacio == "rand") resultadoOperacion = rand();
+
+    if (resultadoOperacion == Infinity){
+        document.body.classList.add("gradient");
+    }
+
+    document.querySelector("#resultat").value= resultadoOperacion;
+}
+
+function suma(a, b){
+    return a+b;
+}
+
+function resta(a, b){
+    if(a>b) return a-b;
+    else return b-a;
+}
+
+function mult(a, b){
+    return a*b;
+}
+
+function div(a, b){
+    return a/b;
+}
+
+function rand(){
+    return parseInt(Math.random()*1000);
+}
+
+var random = function(){
+    var intervalo = setInterval(()=>{
+        var valor = rand();
+        mensaje.textContent = valor;
+        if (valor == 999) clearInterval(intervalo);
+    }, 1);
+}();
